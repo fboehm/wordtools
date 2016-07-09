643cffd575cb66047f24e1005d507e40e3b6f0aa
fboehm <frederick.boehm@gmail.com>
2016-07-09 10:23:08 -0500

    moved helper functions from trump project to wordtools package

--- R/find_date_row.R
+++ R/find_date_row.R
@@ -24,3 +24,34 @@ find_date_row <- function(x, format = "%B %d, %Y"){
 is_date <- function(x, format = "%B %d, %Y"){
   !is.na(lubridate::as_date(x, format = format))
 }
+
+#' Tabulate a vector of dates
+#'
+#' @param directory a character vector specifying path to a directory
+#' @param end_of_article a character vector specifying end of story
+#' @export
+
+tabulate_dates <- function(directory,
+                           end_of_article = "All Rights Reserved"){
+  out <- list()
+  i <- 0
+  for (fn in dir(directory)){
+    i <- i + 1
+    ff <- file.path(directory, fn)
+    out[[i]]<- extract_dates(ff, end_of_article = end_of_article)
+  }
+  foo <- sapply(FUN = as.character, X = out)
+  foo2 <- unlist(foo)
+  dates <- lubridate::as_date(foo2)
+  return(table_dates(dates))
+}
+
+#' Table dates
+#'
+#' @param dates a dates vector
+#' @export
+
+table_dates <- function(dates){
+  tab <- table(dates)
+  return(data.frame(tab))
+}
