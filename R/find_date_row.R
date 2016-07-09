32cb778534b29d6ea1e2fba343fc201359a82290
fboehm <frederick.boehm@gmail.com>
2016-07-09 10:11:55 -0500

    fixed documentation of find_date_row() and is_date()

--- R/find_date_row.R
+++ R/find_date_row.R
@@ -1,15 +1,15 @@
 #' Find the row, for one article, in which the date is listed
 #'
-#'
-#'
-#'
+#' @param x a vector
+#' @return a number, the index of the first entry of x that is a date
+#' @export
 
-find_date_row <- function(x){
+find_date_row <- function(x, format = "%B %d, %Y"){
   indic <- FALSE
   i <- 0
   while(!indic){
     i <- i + 1
-    indic <- is_date(x[i])
+    indic <- is_date(x[i], format = format)
   }
   return(i)
 }
@@ -19,6 +19,7 @@ find_date_row <- function(x){
 #'
 #' @param x an input vector
 #' @return a logical vector
+#' @export
 
 is_date <- function(x, format = "%B %d, %Y"){
   !is.na(lubridate::as_date(x, format = format))
