1aa19bae81b265e98d4bb7444c7022611906299b
fboehm <frederick.boehm@gmail.com>
2016-10-05 20:01:52 -0500

    added function for extracting URLs from a text file

---
+++ R/extract_urls.R
@@ -0,0 +1,12 @@
+#' Extract URLs from a text file that contains database query results (such as Proquest)
+#'
+#' @param file a character vector that specifies the filename
+#'
+
+extract_urls <- function(file){
+  search_results <- readLines(file)
+  url_lines <- search_results[str_detect(string = search_results, pattern = "URL:")]
+  list_urls<- str_split(string = url_lines, pattern = "URL: ")
+  urls <- sapply(FUN = function(x)x[2], X = list_urls)
+  return(urls)
+}
